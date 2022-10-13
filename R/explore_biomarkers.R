4e51f5b2c3ffcbdb2ce99cc3f480914819fd9212
olapuente <o.lapuente.santana@tue.nl>
2022-10-13 21:42:06 +0200

    hacky solution: add @importFrom coin statistic

--- R/explore_biomarkers.R
+++ R/explore_biomarkers.R
@@ -10,7 +10,7 @@
 #' @importFrom utils combn
 #' @importFrom reshape2 melt
 #' @importFrom rstatix wilcox_test wilcox_effsize
-#' @import coin
+#' @importFrom coin statistic
 #' @importFrom ggrepel geom_text_repel
 #' @import ggplot2
 #' @importFrom ggpubr ggarrange annotate_figure text_grob
